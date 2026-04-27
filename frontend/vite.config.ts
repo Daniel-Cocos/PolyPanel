c4b90bab19d622cdbee8270f7f82af849b0eae96
Aleks Tacconi <aleks.tacconi@gmail.com>
2026-04-27 11:30:10 +0100

    fix(frontend): deploy under new name

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  base: process.env.VITE_PUBLIC_BASE ?? '/GreenTech/',
+  base: process.env.VITE_PUBLIC_BASE ?? '/PolyPanel/',
   plugins: [react()],
   server: {
     proxy: {
